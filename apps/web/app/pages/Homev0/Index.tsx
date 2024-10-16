afdb72812ac711896db72a72c371a59798399d09
khalfaaymen <khalfaaymen141@gmail.com>
2024-10-16 11:14:40 +0100

    update

--- apps/web/app/pages/Homev0/Index.tsx
+++ apps/web/app/pages/Homev0/Index.tsx
@@ -15,7 +15,7 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <main>
-        <Hero />
+        {/* <Hero />
         <PopularCategories />
         <FreelancerCard/>
         <FeaturedFreelancers />
@@ -26,7 +26,7 @@ const Index = () => {
         <PromotionalBanner />
         <RecentProjects />
         <NewsletterSignUp />
-        <MobileAppPromo />
+        <MobileAppPromo /> */}
       </main>
       <Footer />
     </div>
