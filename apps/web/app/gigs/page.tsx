f7ee0f887c1a498fcdcfb06864cf58e7c2eed23f
AKM <Azyz>
2024-10-17 17:20:53 +0100

    before release 1

--- apps/web/app/gigs/page.tsx
+++ apps/web/app/gigs/page.tsx
@@ -39,12 +39,12 @@ const Search: React.FC = () => {
         } else if (!query && categoryId) {
           const services: any = await getServicesByCategory(categoryId);
           setFeaturedServices(services);
-        }  else {
+        } else {
           const services: any = await getAllServices();
           setFeaturedServices(services);
         }
       } catch (e) {
-        console.log(e);
+        // console.log(e);
       } finally {
         setLoading(false);
       }
@@ -54,52 +54,52 @@ const Search: React.FC = () => {
   }, [query]);
 
   return (
-    <div className="bg-secondary/10 py-16">
-      <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-bold mb-8 text-center">Gigs en vedette</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <div className="py-16 bg-secondary/10">
+      <div className="container px-6 mx-auto">
+        <h2 className="mb-8 text-3xl font-bold text-center">Gigs en vedette</h2>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           {loading && (
-            <div className="bg-background p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow">
+            <div className="p-6 transition-shadow rounded-lg shadow-md bg-background hover:shadow-lg">
               Loading...
             </div>
           )}
-          {!loading && featuredServices.length > 0 ? (
-            featuredServices.map((service) => (
-              <div
-                key={service.id}
-                className="bg-background p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow"
-              >
-                <h3 className="font-bold mb-2">{service.name}</h3>
-                <p className="text-sm text-muted-foreground mb-2">
-                  {service.description}
-                </p>
-                <div className="flex items-center mb-2">
-                  <Star
-                    className="text-yellow-400 fill-yellow-400 mr-1"
-                    size={16}
-                  />
-                  <span>
-                    {service.ratings && service.ratings.length > 0
-                      ? (
-                          service.ratings.reduce(
-                            (acc, rating) => acc + rating.rating,
-                            0,
-                          ) / service.ratings.length
-                        ).toFixed(1)
-                      : "N/A"}
-                  </span>
+          {!loading && featuredServices.length > 0
+            ? featuredServices.map((service) => (
+                <div
+                  key={service.id}
+                  className="p-6 transition-shadow rounded-lg shadow-md bg-background hover:shadow-lg"
+                >
+                  <h3 className="mb-2 font-bold">{service.name}</h3>
+                  <p className="mb-2 text-sm text-muted-foreground">
+                    {service.description}
+                  </p>
+                  <div className="flex items-center mb-2">
+                    <Star
+                      className="mr-1 text-yellow-400 fill-yellow-400"
+                      size={16}
+                    />
+                    <span>
+                      {service.ratings && service.ratings.length > 0
+                        ? (
+                            service.ratings.reduce(
+                              (acc, rating) => acc + rating.rating,
+                              0,
+                            ) / service.ratings.length
+                          ).toFixed(1)
+                        : "N/A"}
+                    </span>
+                  </div>
+                  <p className="font-bold text-primary">{service.price}</p>
+                  <Link href={`/service/${service.id}`}>
+                    <Button className="w-full mt-4">View Details</Button>
+                  </Link>
                 </div>
-                <p className="font-bold text-primary">{service.price}</p>
-                <Link href={`/service/${service.id}`}>
-                  <Button className="mt-4 w-full">View Details</Button>
-                </Link>
-              </div>
-            ))
-          ) : !loading && (
-            <div className="bg-background p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-              No services found.
-            </div>
-          )}
+              ))
+            : !loading && (
+                <div className="p-6 transition-shadow rounded-lg shadow-md bg-background hover:shadow-lg">
+                  No services found.
+                </div>
+              )}
         </div>
       </div>
     </div>
