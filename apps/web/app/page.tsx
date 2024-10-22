55c05435b982568049f008a53e40233cb1653496
kuroxdev <ijendoubi19@gmail.com>
2024-10-22 09:45:45 +0100

    solve conflict

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-// import CenterWrapper from "./layout/Center";
-import CategoriesBar from "./pages/Home/CategoriesBar";
-// import LoginCard from "./pages/HomePage/LoginCard";
-// import MainNavbar from "./pages/HomePage/MainNavbar";
-import HomePage from "./pages/Home/Index";
-// import Image from "next/image";
-=======
 import Hero from "@/components/hero";
->>>>>>> e6e740c110d3d572cc2d37b18186e3e88be20a79
 
 export default async function Index() {
   return (
