18d92b92886f64e0fdf392c47df905b4d78639cd
kuroxdev <ijendoubi19@gmail.com>
2024-10-28 01:51:29 +0100

    edit profile & contact public profile

--- apps/web/utils/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-export const formatTND = (amount: number) => {
-  return new Intl.NumberFormat("fr-TN", {
-    style: "currency",
-    currency: "TND",
-    minimumFractionDigits: 3,
-    maximumFractionDigits: 3,
-  }).format(amount);
-};
