49ba90c48239bfe63d5946d4a9140e23b512e075
AKM <Azyz>
2024-11-05 11:32:18 +0100

    preparation for push

--- apps/web/app/pages/__footer/index.tsx
+++ apps/web/app/pages/__footer/index.tsx
@@ -1,12 +1,6 @@
 import MainPart from "@/app/pages/__footer/MainPart";
-import CopyrightPart from "./CopyrightPart";
 const Footer = ({}) => {
-  return (
-    <footer className="flex flex-col justify-center text-xs text-center md:text-sm">
-      <MainPart />
-      <CopyrightPart />
-    </footer>
-  );
+  return <MainPart />;
 };
 
 export default Footer;
